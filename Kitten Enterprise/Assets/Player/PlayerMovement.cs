35839585ceb995107e10b5efbeda06c5845b5409
Adrienn Tindall <33725376+aidenfrog@users.noreply.github.com>
2022-02-07 20:47:13 -0600

    Player Movement Prototype
    -Added the following class:
    *PlayerMovement
      *Is able to take input from WASD and ArrowKeys using unity input system 1.3 to move the player in the xz plane. The forwards direction is based on the forwards of the camera, allowing for free camera movement later on

---
+++ Kitten Enterprise/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private Rigidbody playerRigidbody;
+
+    private Plane movementPlane = new Plane(new Vector3(0, 1, 0), Vector3.zero);
+
+    private Vector3 forwardsVector, rightVector;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    //setting up movement so we don't have to compute this literally every frame
+    public void startMovement()
+    {
+        forwardsVector = Camera.main.transform.forward;
+        forwardsVector = movementPlane.ClosestPointOnPlane(forwardsVector).normalized;
+        rightVector = -Vector3.Cross(forwardsVector, movementPlane.normal);
+    }
+
+    public void OnMove(InputValue input)
+    {
+        startMovement();
+
+        Vector2 inputVector = input.Get<Vector2>();
+        Vector3 movementVector = inputVector.x * rightVector + inputVector.y * forwardsVector;
+       
+        playerRigidbody.AddForce(-playerRigidbody.velocity, ForceMode.VelocityChange);
+        
+        playerRigidbody.AddForce(movementVector * movementSpeed, ForceMode.VelocityChange);
+    }
+}
